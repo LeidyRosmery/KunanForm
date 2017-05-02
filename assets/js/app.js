01329270c803325376c6449c5e9022bc2711e8d6
LeidyRosmery <rosm3rita@hotmail.com>
2017-05-01 22:30:52 -0500

    funciona

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,21 @@
-document.getElementById("Guardar",function(){
+var input=document.getElementsByTagName("input");
+var guardar=document.getElementById("Guardar");
+guardar.addEventListener('click',function(e){
+event.preventDefault() ;
 
+var expNombre=/([A-Z]){1}[a-z]+[\s]([A-Z]){1}[a-z]+/;
+var expNombre1=/([A-Z]){1}[a-z]+/;
+var expNacimiento=/(0[1-9]|1[0-9]|2[0-9]|3[01])[/](0[1-9]|1[012])[/][0-9]{4}/;
+if(!expNombre.test(input[0].value)){
+  console.log("ingrese correctamente los nombres");
+}
+if(!expNacimiento.test(input[1].value)){
+  console.log("ingrese fecha de nacimiento correctamente");
+}
 });
+
+function getRandomInt(min, max) {
+  return  valor=Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log(getRandomInt(0.5,15.7));
